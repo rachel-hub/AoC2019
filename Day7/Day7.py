4cea25c5810486da277a8fc0186aec818ff4e8e5
rachel-hub <rachel.tadd@outlook.com>
2019-12-11 22:39:25 +0000

    Further work on day 7 part 2. Intcode2 created and adjusted_amplifiers coded but not tested.

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -11,8 +11,8 @@ InputData = np.loadtxt("aoc2019_day7_input.txt",
                        dtype='int64',
                        delimiter=",")
 
-# TestInput1 = np.array([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
-# InputData = TestInput1.copy()
+TestInput1 = np.array([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
+InputData = TestInput1.copy()
 
 
 # About intcode programs:
@@ -43,11 +43,8 @@ InputData = np.loadtxt("aoc2019_day7_input.txt",
 # It might be sensible to parametrise by the number of parameters to help future proof
 
 
-def intcode(input_data,phase_value,input_value,output_parameters=[]):
+def intcode(input_data,phase_value,input_value,return_value = np.nan,phase_use_counter = 0,current_index = 0):
     this_input = input_data.copy()
-    return_value = np.nan
-    phase_use_counter = 0
-    current_index = 0
     max_index = len(this_input) - 1
     current_op_mode_code = this_input[current_index].copy()
     current_mode_codes = current_op_mode_code // 100
@@ -119,7 +116,6 @@ def intcode(input_data,phase_value,input_value,output_parameters=[]):
         elif current_op_code == 4:
             print(parameter1)
             return_value = parameter1.copy()
-            output_parameters.append(return_value)
             current_index += 2
 
         elif current_op_code == 5:
@@ -154,6 +150,123 @@ def intcode(input_data,phase_value,input_value,output_parameters=[]):
             break
 
 
+def intcode2(input_data,phase_value,input_value,return_value = np.nan,phase_use_counter = 0,current_index = 0):
+    this_input = input_data.copy()
+    max_index = len(this_input) - 1
+    current_op_mode_code = this_input[current_index].copy()
+    current_mode_codes = current_op_mode_code // 100
+    current_op_code = current_op_mode_code - current_mode_codes * 100
+    current_mode_code_3 = current_mode_codes // 100
+    current_mode_code_2 = (current_mode_codes - current_mode_code_3 * 100) // 10
+    current_mode_code_1 = (current_mode_codes - current_mode_code_3 * 100 - current_mode_code_2 * 10)
+
+    # Check mode codes are valid
+    if not((current_mode_code_1 == 0) or (current_mode_code_1 == 1)):
+        return "Error with mode code 1"
+    if not((current_mode_code_2 == 0) or (current_mode_code_2 == 1)):
+        return "Error with mode code 2"
+    if not((current_mode_code_3 == 0) or (current_mode_code_3 == 1)):
+        return "Error with mode code 3"
+
+    while current_op_code != 99:
+        current_op_mode_code = this_input[current_index].copy()
+        current_mode_codes = current_op_mode_code // 100
+        current_op_code = current_op_mode_code - current_mode_codes * 100
+        current_mode_code_3 = current_mode_codes // 100
+        current_mode_code_2 = (current_mode_codes - current_mode_code_3 * 100) // 10
+        current_mode_code_1 = (current_mode_codes - current_mode_code_3 * 100 - current_mode_code_2 * 10)
+
+        # Check mode codes are valid
+        if not ((current_mode_code_1 == 0) or (current_mode_code_1 == 1)):
+            return "Error with mode code 1"
+        if not ((current_mode_code_2 == 0) or (current_mode_code_2 == 1)):
+            return "Error with mode code 2"
+        if not ((current_mode_code_3 == 0) or (current_mode_code_3 == 1)):
+            return "Error with mode code 3"
+
+        index1 = this_input[current_index + 1]
+        index2 = this_input[current_index + 2]
+        insert_index = this_input[current_index + 3]
+
+        try:
+            parameter1 = (1 - current_mode_code_1)*this_input[min(max(0,index1),max_index)] + current_mode_code_1*index1
+        except:
+            pass
+        try:
+            parameter2 = (1 - current_mode_code_2)*this_input[min(max(0,index2),max_index)] + current_mode_code_2*index2
+        except:
+            pass
+
+        if current_op_code == 1:
+            # print("OpCode identified as 1")
+            sum_val = parameter1 + parameter2
+            this_input[insert_index] = sum_val
+            current_index += 4
+
+        elif current_op_code == 2:
+            # print("OpCode identified as 2")
+            mult_val = parameter1 * parameter2
+            this_input[insert_index] = mult_val
+            current_index += 4
+
+        elif current_op_code == 3:
+            if phase_use_counter == 0:
+                this_input[index1] = phase_value
+                phase_use_counter += 1
+            else:
+                this_input[index1] = input_value
+            current_index += 2
+
+        elif current_op_code == 4:
+            print(parameter1)
+            return_value = parameter1.copy()
+            current_index += 2
+            return return_value, current_op_code, current_index, phase_use_counter, this_input
+
+        elif current_op_code == 5:
+            if parameter1 != 0:
+                current_index = parameter2
+            else:
+                current_index += 3
+
+        elif current_op_code == 6:
+            if parameter1 == 0:
+                current_index = parameter2
+            else:
+                current_index += 3
+
+        elif current_op_code == 7:
+            if parameter1 < parameter2:
+                this_input[insert_index] = 1
+            else:
+                this_input[insert_index] = 0
+            current_index += 4
+
+        elif current_op_code == 8:
+            if parameter1 == parameter2:
+                this_input[insert_index] = 1
+            else:
+                this_input[insert_index] = 0
+            current_index += 4
+
+        else:
+            print(current_op_code)
+            print("Unknown OpCode identified")
+            break
+
+    if current_op_code == 99:
+        return return_value, current_op_code
+
+
+def complete_intcode2_runner(input_data,phase_value,input_value):
+    outputs =  intcode2(input_data,phase_value,input_value)
+    while outputs[1] != 99:
+        outputs = intcode2(outputs[4],phase_value,outputs[0],outputs[0],outputs[3],outputs[2])
+    return outputs[0]
+
+
+
+
 def amplifiers(input_data,phase_value_a,phase_value_b,phase_value_c,phase_value_d,phase_value_e,input_value):
     amplifier_a_value = intcode(input_data, phase_value_a, input_value)
     amplifier_b_value = intcode(input_data, phase_value_b, amplifier_a_value)
@@ -163,41 +276,71 @@ def amplifiers(input_data,phase_value_a,phase_value_b,phase_value_c,phase_value_
     return amplifier_e_value
 
 
+def amplifiers2(input_data,phase_value_a,phase_value_b,phase_value_c,phase_value_d,phase_value_e,input_value):
+    amplifier_a_value = complete_intcode2_runner(input_data, phase_value_a, input_value)
+    print(amplifier_a_value)
+    amplifier_b_value = complete_intcode2_runner(input_data, phase_value_b, amplifier_a_value)
+    amplifier_c_value = complete_intcode2_runner(input_data, phase_value_c, amplifier_b_value)
+    amplifier_d_value = complete_intcode2_runner(input_data, phase_value_d, amplifier_c_value)
+    amplifier_e_value = complete_intcode2_runner(input_data, phase_value_e, amplifier_d_value)
+    return amplifier_e_value
+
 def adjusted_amplifiers(input_data,phase_value_a,phase_value_b,phase_value_c,phase_value_d,phase_value_e,input_value):
+    amplifier_a_opcode = 0
+    amplifier_b_opcode = 0
+    amplifier_c_opcode = 0
+    amplifier_d_opcode = 0
+    amplifier_e_opcode = 0
+
     amplifier_a_parameters = []
     amplifier_b_parameters = []
     amplifier_c_parameters = []
     amplifier_d_parameters = []
-    amplifier_e_parameters = [input_value]
+    amplifier_e_parameters = [(input_value, amplifier_e_opcode, 0, 0, input_data)]
 
-    amplifier_a_output = np.nan
-    amplifier_b_output = np.nan
-    amplifier_c_output = np.nan
-    amplifier_d_output = np.nan
-    amplifier_e_output = np.nan
-
-    while np.isnan(amplifier_a_output) or np.isnan(amplifier_b_output) or np.isnan(amplifier_c_output) or \
-        np.isnan(amplifier_d_output) or np.isnan(amplifier_e_output):
+    while (amplifier_a_opcode != 99) or (amplifier_b_opcode != 99) or (amplifier_c_opcode != 99) or \
+        (amplifier_d_opcode != 99) or (amplifier_e_opcode != 99):
         if len(amplifier_a_parameters) > 0:
-            amplifier_a_value = amplifier_a_parameters[0]
+            this_parameter = amplifier_a_parameters[0]
+            amplifier_b_outputs = intcode2(this_parameter[4], phase_value_b, this_parameter[0], this_parameter[0],
+                                           this_parameter[3], this_parameter[2])
+            amplifier_b_parameters.append(amplifier_b_outputs)
             amplifier_a_parameters = amplifier_a_parameters[1:]
-            amplifier_b_output = intcode(input_data, phase_value_b, amplifier_a_value, amplifier_b_parameters)
+            amplifier_b_opcode = amplifier_b_outputs[1]
+
         if len(amplifier_b_parameters) > 0:
-            amplifier_b_value = amplifier_b_parameters[0]
+            this_parameter = amplifier_b_parameters[0]
+            amplifier_c_outputs = intcode2(this_parameter[4], phase_value_c, this_parameter[0], this_parameter[0],
+                                           this_parameter[3], this_parameter[2])
+            amplifier_c_parameters.append(amplifier_c_outputs)
             amplifier_b_parameters = amplifier_b_parameters[1:]
-            amplifier_c_output = intcode(input_data, phase_value_c, amplifier_b_value, amplifier_c_parameters)
+            amplifier_c_opcode = amplifier_c_outputs[1]
+
         if len(amplifier_c_parameters) > 0:
-            amplifier_c_value = amplifier_c_parameters[0]
+            this_parameter = amplifier_c_parameters[0]
+            amplifier_d_outputs = intcode2(this_parameter[4], phase_value_d, this_parameter[0], this_parameter[0],
+                                           this_parameter[3], this_parameter[2])
+            amplifier_d_parameters.append(amplifier_d_outputs)
             amplifier_c_parameters = amplifier_c_parameters[1:]
-            amplifier_d_output = intcode(input_data, phase_value_d, amplifier_c_value, amplifier_d_parameters)
+            amplifier_d_opcode = amplifier_d_outputs[1]
+
         if len(amplifier_d_parameters) > 0:
-            amplifier_d_value = amplifier_d_parameters[0]
+            this_parameter = amplifier_d_parameters[0]
+            amplifier_e_outputs = intcode2(this_parameter[4], phase_value_e, this_parameter[0], this_parameter[0],
+                                           this_parameter[3], this_parameter[2])
+            amplifier_e_parameters.append(amplifier_e_outputs)
             amplifier_d_parameters = amplifier_d_parameters[1:]
-            amplifier_e_output = intcode(input_data, phase_value_e, amplifier_d_value, amplifier_e_parameters)
+            amplifier_e_opcode = amplifier_e_outputs[1]
+            amplifier_e_value = amplifier_e_outputs[0]
+
         if len(amplifier_e_parameters) > 0:
-            amplifier_e_value = amplifier_e_parameters[0]
+            this_parameter = amplifier_e_parameters[0]
+            amplifier_a_outputs = intcode2(this_parameter[4], phase_value_a, this_parameter[0], this_parameter[0],
+                                           this_parameter[3], this_parameter[2])
+            amplifier_a_parameters.append(amplifier_a_outputs)
             amplifier_e_parameters = amplifier_e_parameters[1:]
-            amplifier_a_output = intcode(input_data, phase_value_a, amplifier_e_value, amplifier_a_parameters)
+            amplifier_a_opcode = amplifier_a_outputs[1]
+
     return amplifier_e_value
 
 
@@ -220,8 +363,31 @@ for perm in perms:
 
 MaxOutputSignal
 
+perms2 = permutations([0,1,2,3,4])
+MaxOutputSignal2 = 0
+
+for perm in perms2:
+    phase_a = perm[0]
+    phase_b = perm[1]
+    phase_c = perm[2]
+    phase_d = perm[3]
+    phase_e = perm[4]
+    print(phase_a,phase_b,phase_c,phase_d,phase_e)
+    output_signal2 = amplifiers2(InputData,phase_a,phase_b,phase_c,phase_d,phase_e,0)
+    if output_signal2 > MaxOutputSignal2:
+        MaxOutputSignal2 = output_signal2.copy()
+        MaxPermutation2 = perm
+
+MaxOutputSignal2
+
 # Part 2
 
+# Intcode2 now is running and gives sensible output for part1
+# Try using this for part2 - adjusted_amplifiers is a preliminary structure, just needs testing and debugging
+
+# General thoughts on approach:
+
+# Append output to lists, check their length run non-zero list and depend from the the input used
 # Need to handle passing parameters between intcode and others
 # Probably want to return values after step 4 with some other parameters to help it resume
 # Likely all those set at the start - put them in as inputs with default values instead
